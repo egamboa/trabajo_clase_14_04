d610453cceca7b6e9abc5174a4476039b7f7ac69
Esteban Gamboa <estebangam1@gmail.com>
2018-04-14 13:26:19 -0600

    Initial Commit

---
+++ shakemagic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shakemagic
+//
+//  Created by Andrés Gamboa on 4/14/18.
+//  Copyright © 2018 Andrés Gamboa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
